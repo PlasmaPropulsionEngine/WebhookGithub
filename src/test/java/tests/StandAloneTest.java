aae490516cb7321cb395a48cbc81c5d0d67f5ae3
karan <karansalve13@gmail.com>
2024-11-23 16:26:48 +0530

    pom

--- src/test/java/tests/StandAloneTest.java
+++ src/test/java/tests/StandAloneTest.java
@@ -17,7 +17,7 @@ import com.github.dockerjava.api.command.WaitContainerCmd;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class StandAloneTest {
-//this is stand alone test for pageobject 
+//this is stand alone test for pageobject model
 	public static void main(String[] args) {
 
 
